c46ecc49b74ae6dd65a435f05bd3f9c6347a92fd
matryt <mathieu.cuvelier@outlook.fr>
2023-05-19 19:15:28 +0200

    Style modifs

--- main_affichage.py
+++ main_affichage.py
@@ -1,27 +1,19 @@
 import pygame
-import math
 from pygame.locals import *
 pygame.init()
 
-running = True 
+running = True
 game = False
 
-    
-    
 clock = pygame.time.Clock()
-while running :
+while running:
     pygame.display.set_caption("Calcul mental ")
-    fenetre = pygame.display.set_mode((0,0), (pygame.RESIZABLE))
+    fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
     fenetre.convert()
     pygame.display.update()
 
     pygame.display.update()
     for event in pygame.event.get():
-        if event.type ==KEYDOWN:
-            if event.key==K_ESCAPE:
-                pygame.quit()
-                running= False
-                
-                
-            
-
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            pygame.quit()
+            running = False
