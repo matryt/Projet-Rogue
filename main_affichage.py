2a0a6f593b7632399e5140a3b3c6b7ac2ab446ae
LoicL832 <loiclamour0@gmail.com>
2023-05-20 15:35:54 +0200

    aff color text

--- main_affichage.py
+++ main_affichage.py
@@ -10,13 +10,9 @@ pygame.display.set_caption("DONGEON MASTER")
 fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
 while running:
     fenetre.fill([0,0,128])
-    
-    white = (255, 255, 255) 
-    green = (0, 255, 0) 
-    blue = (0, 0, 128) 
     varText = "Press ESCAPE to quit"
     font = pygame.font.Font('freesansbold.ttf', 40)
-    text = font.render(varText, True, 'white')
+    text = font.render(varText, True, 'black')
     textRect = text.get_rect()
     textRect.center = (210,fenetre.get_size()[1]-25)
     fenetre.blit(text, textRect)
