00bf1305b0d659fb15d9dd6d3f0d9ce99a26005e
LoicL832 <loiclamour0@gmail.com>
2023-05-20 12:51:48 +0200

    affichage et assets

--- main_affichage.py
+++ main_affichage.py
@@ -1,10 +1,8 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-
 running = True
 game = False
-
 clock = pygame.time.Clock()
 while running:
     pygame.display.set_caption("DONGEON MASTER")
@@ -13,5 +11,8 @@ while running:
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
+            pygame.quit()
+            running = False
+        if event.type==pygame.quit():
             pygame.quit()
             running = False
