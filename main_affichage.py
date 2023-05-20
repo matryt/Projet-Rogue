69f0aea437659be17d8380007727d07be42e084f
LoicL832 <loiclamour0@gmail.com>
2023-05-20 14:50:35 +0200

    background

--- main_affichage.py
+++ main_affichage.py
@@ -6,11 +6,11 @@ running = True
 game = False
 
 clock = pygame.time.Clock()
+pygame.display.set_caption("DONGEON MASTER")
+fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
 while running:
-    pygame.display.set_caption("DONGEON MASTER")
-    fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
-    fenetre.convert()
-    pygame.display.update()
+    fenetre.fill([0,0,128])
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
