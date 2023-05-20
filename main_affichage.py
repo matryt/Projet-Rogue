5835812c5c5582e88a70780c0fed8c47d69cfc59
LoicL832 <loiclamour0@gmail.com>
2023-05-20 14:54:49 +0200

    dsdf

--- main_affichage.py
+++ main_affichage.py
@@ -11,6 +11,14 @@ fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
 while running:
     fenetre.fill([0,0,128])
     pygame.display.flip()
+    white = (255, 255, 255) 
+    green = (0, 255, 0) 
+    blue = (0, 0, 128) 
+    varText = ""
+    font = pygame.font.Font('freesansbold.ttf', 64)
+    text = font.render(varText, True, green,blue)
+    textRect = text.get_rect()
+    textRect.center = (960, 540)
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
