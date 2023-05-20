f8ee6700ee4e440e8c53c7bbcf7bc483790561e1
LoicL832 <loiclamour0@gmail.com>
2023-05-20 15:15:50 +0200

    text to quit

--- main_affichage.py
+++ main_affichage.py
@@ -10,15 +10,18 @@ pygame.display.set_caption("DONGEON MASTER")
 fenetre = pygame.display.set_mode((0, 0), (pygame.RESIZABLE))
 while running:
     fenetre.fill([0,0,128])
-    pygame.display.flip()
+    
     white = (255, 255, 255) 
     green = (0, 255, 0) 
     blue = (0, 0, 128) 
-    varText = ""
-    font = pygame.font.Font('freesansbold.ttf', 64)
-    text = font.render(varText, True, green,blue)
+    varText = "Press ESCAPE to quit"
+    font = pygame.font.Font('freesansbold.ttf', 40)
+    text = font.render(varText, True, 'white')
     textRect = text.get_rect()
-    textRect.center = (960, 540)
+    textRect.center = (210,fenetre.get_size()[1]-25)
+    fenetre.blit(text, textRect)
+    pygame.display.flip()
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
