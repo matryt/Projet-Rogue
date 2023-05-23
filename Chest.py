21df77880af0dfa9b8187729178294f0014e1bfd
leolhuile <leonard.lucenay@etu.univ-cotedazur.fr>
2023-05-23 20:04:54 +0200

    coffre v3

--- Chest.py
+++ Chest.py
@@ -4,6 +4,7 @@ import random
 
 theGame = importlib.import_module("theGame")
 
+chestopened = False
 
 class Chest(Element.Element):
 	"""
@@ -20,18 +21,23 @@ class Chest(Element.Element):
 		super().__init__("Chest", "M")
 		
 	def meet(self,hero):
-		"""Permet d'ouvrir le coffre à condition d'avoir une clé'"""
+		"""Permet d'ouvrir le coffre à condition d'avoir une clé'""" 
 		for object in hero._inventory:
 			if  object._name == "key":
-				hero._equipments.pop(object)
+				self.chestopened = True
 				Totalequipments = theGame.theGame().equipments 
 				theGame.theGame().addMessage(f"you open the chest with the key")
 				theGame.theGame().addMessage(f"wowie, you just gained gold and stuff : ")
 				hero.GoldCount += random.randint(5*theGame.theGame()._level,25*theGame.theGame()._level)
 				for rareté in Totalequipments:
 					if rareté == 2*theGame.theGame()._level:
-						hero.take(Totalequipments[rareté][random.randint(0,len(Totalequipments[rareté]))])
+						hero.take(Totalequipments[rareté][random.randint(0,len(Totalequipments[rareté])-1)]) 
+						hero._inventory.remove(object)
 
-
+		if self.chestopened:
+			theGame.theGame().addMessage(f"the chest has been opened")
+			return
 		theGame.theGame().addMessage(f"it seems that a key is needed to open this chest")
+
+		#note: on supprimera plutot le coffre, c'est plus simple mais dans l'ideal on preferera le laisser sur la map,ouvert.
 		
